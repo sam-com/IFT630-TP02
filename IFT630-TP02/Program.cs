77f12727d103b9ccc3cae0f4c2d0e43bbcfd708f
PhilipeHamel <philipe.hamel@usherbrooke.ca>
2017-08-05 13:38:20 -0400

    no message

--- IFT630-TP02/Program.cs
+++ IFT630-TP02/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MPI;
 
 namespace IFT630_TP02
 {
@@ -10,6 +11,10 @@ namespace IFT630_TP02
     {
         static void Main(string[] args)
         {
+            using (new MPI.Environment(ref args))
+            {
+                Console.WriteLine("Hello, World! from rank " + Communicator.world.Rank + " (running on " + MPI.Environment.ProcessorName + ")");
+            }
         }
     }
 }
