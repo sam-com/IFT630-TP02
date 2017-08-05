f1701ea6ca07968b6ea4c5d34d48437ff9bf9eff
sam-com <samuelcomeau@gmail.com>
2017-08-05 12:13:47 -0400

    inital commit

---
+++ IFT630-TP02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFT630_TP02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
